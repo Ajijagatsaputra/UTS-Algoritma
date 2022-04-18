c83fc6bfa6fd053de4e242aca56c215dd5a76b9c
Ajijagatsaputra <93568847+Ajijagatsaputra@users.noreply.github.com>
2022-04-18 14:18:46 +0700

    Add files via upload

---
+++ Facebook.java
@@ -0,0 +1,11 @@
+package Facebook;
+
+public class FB{
+    public int JumlahLike;
+    public int JumlahKomen;
+
+    public FB(int JumlahLike, int JumlahKomen){
+        this.JumlahLike = like;
+        this.JumlahKomen = komen;
+    }  
+}
---
+++ facebook.java
@@ -0,0 +1,17 @@
+public class facebook {
+    private static void Facebook(){
+        System.out.println("---------------");
+        System.out.println(" BANK HARBER - CABANG TEGAL");
+        System.out.println();
+    }
+}
+public static void main(String[] args) {
+    String nokar;
+    String norek;
+    String nama;
+    String pin;
+    
+    nokar = "ABCDE";
+    norek = "087687";
+    nama = "donald";
+}
